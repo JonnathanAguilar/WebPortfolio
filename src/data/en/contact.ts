16a2723f889ab533b16934f64d4197ceba1c3626
JonnathanAguilar <jonnathan.aguilarl@ucuenca.edu.ec>
2026-03-27 21:32:46 -0500

    bug: fix mail sending

---
+++ src/data/en/contact.ts
@@ -0,0 +1,7 @@
+export const contactData = {
+  personal_email: "jaguilarlalvay@gmail.com",
+  work_email: "codigomaestro.it@gmail.com",
+  phone: "+593 98 779 8311",
+  address: "010114, Campus Balzay, Cuenca, Ecuador",
+  orcid: "0009-0008-0363-2245"
+};
